c8ad8dd18bc5969568d89ff85f4dcfcb5768104e
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-04-30 22:56:14 -0300

    API resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Product;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\Product as ProductResource;
+use App\Http\Resources\Products as ProductsResource;
+
 class ProductController extends Controller
 {
     /**
@@ -14,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return new ProductsResource(Product::all());
     }
 
     /**
@@ -46,7 +49,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
